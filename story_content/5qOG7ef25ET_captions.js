a7d53e32759dc3bf9eeefb8f348ac84c3b369962
Tara Swenson <taradswenson@gmail.com>
2022-07-20 10:26:04 -0700

    Configure-WinRM-Security

---
+++ story_content/5qOG7ef25ET_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.38.21861.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:02.455%0D%0ATake%20note%20of%20where%20Puppet%20creates%20modules.%0D%0A%0D%0A"}')
